740c452065cd11d63364e5f7da23785e5806e523
danuki.nqh <nguyenquanghuylt2002@gmail.com>
2022-12-29 17:57:36 +0700

    Upload 29/12/2022

--- js/script.js
+++ js/script.js
@@ -8,17 +8,4 @@ window.addEventListener('load', function(){
             homeMenu.classList.toggle('active');
         });
     });
-    /**
-     * Button next page
-     */
-    document.querySelector('#change-page').addEventListener('click', function(){
-        if(this.classList.contains('active') == true){
-            this.classList.add('not-active');
-        }else{
-            this.classList.remove('not-active');
-        }
-        this.classList.toggle('active');
-        document.querySelector('#home').classList.toggle('active');
-        document.querySelector('#section').classList.toggle('active');
-    })
 });
