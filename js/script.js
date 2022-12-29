ca7d8af9520d87a8279d9f8e8402c796067f04f5
danuki.nqh <nguyenquanghuylt2002@gmail.com>
2022-12-29 22:42:19 +0700

    Upload 29/12/2022

--- js/script.js
+++ js/script.js
@@ -8,4 +8,33 @@ window.addEventListener('load', function(){
             homeMenu.classList.toggle('active');
         });
     });
+    /**
+     * Close menu when scrolling
+     */
+    window.addEventListener('scroll', function(){
+        if(window.scrollY > 100){
+            homeMenu.classList.remove('active');
+        }
+    });
+    /**
+     * Close menu when click link menu
+     */
+    document.querySelectorAll('#linkMenu li').forEach(linkMenu =>{
+        linkMenu.addEventListener('click', function(){
+            homeMenu.classList.remove('active');
+        })
+    });
+});
+/**
+ * Animate on scroll
+ */
+let text = document.getElementById('text');
+let imgct = document.getElementById('image-content');
+window.addEventListener('scroll', function(){
+    let position = window.scrollY;
+    let s = 1000;
+    let o = 250;
+    text.style.transform = 'translateX(' + (-position*0.6) + 'px)';
+    text.style.opacity = (o - position)/o;
+    imgct.style.transform = 'scale(' + (position+s)/s + ')';
 });
